93d067e855109ca278308492ae7f799006e85711
Riuki <nodarchinchaladze6@gmail.com>
2026-03-14 13:36:20 +0400

    quiz

---
+++ quiz.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+void reversearr(int arr[], int n) {
+int left = 0, right = n - 1;
+
+    while (left < right) {
+     int temp = arr[left];
+     arr[left] = arr[right];
+     arr[right] = temp;
+
+     left++;
+     right--;
+ }
+
+}
+
+int main() {
+int arr[]={1,2,3,4,5};
+int n = sizeof(arr) / sizeof(arr[0]);
+ reversearr(arr, n);
+for(int i=0; i < n; i++) {
+
+cout << arr[i] << " ";
+
+}
+return 0;
+}
---
+++ quiz2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+void swap(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+int main() {
+int a, b;
+
+cout << "input the value of a" << endl;
+cin >> a;
+cout << "input the value of b" << endl;
+cin >> b;
+
+swap(&a, &b);
+cout << "a= " << a << endl;
+cout << "b= " << b << endl;
+
+}
